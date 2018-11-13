28025bb289da75767561e1866be36f23c6da5d6d
Sach97 <sacha.arbonel@hotmail.fr>
2018-11-13 12:32:36 +0100

    add article struct

--- search/main.go
+++ search/main.go
@@ -4,9 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
-	goqu "gopkg.in/doug-martin/goqu.v5"
 	_ "gopkg.in/doug-martin/goqu.v5/adapters/postgres"
 )
 
@@ -33,16 +33,28 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Successfully connected!")
+	defer pgDb.Close()
 
-	db := goqu.New("postgres", pgDb)
+	type Article struct {
+		Title       string    `db:"title"`
+		PublishDate time.Time `db:"publish_date"`
+		Summary     string    `db:"summary"`
+		TopImage    string    `db:"top_image"`
+	}
+
+	//db := goqu.New("postgres", pgDb)
 
 	// 	SELECT *
 	// FROM products
 	// WHERE to_tsvector('english', product_name) @@ to_tsquery('english', 'hello');
-	product_name := "product_name"
-	sql, _, _ := db.From("products").Where(
-		goqu.L("to_tsvector(?, ?) @@ to_query(?, ?)", "\"english\"", product_name, "\"english\"", "hello"),
-	).ToSql()
-	fmt.Println(sql)
+	// product_name := "product_name"
+	// sql, _, _ := db.From("products").Where(
+	// 	goqu.L("to_tsvector(?, ?) @@ to_query(?, ?)", "\"english\"", product_name, "\"english\"", "hello"),
+	// ).ToSql()
+	// fmt.Println(sql)
+	// productName := "product_name"
+	// sql, _, _ := db.From("products").Prepared(true).Where(
+	// 	goqu.L("to_tsvector(?, ?) @@ to_query(?, ?)", "\"english\"", productName, "\"english\"", "hello"),
+	// ).ScanStructs(&items)
 
 }
